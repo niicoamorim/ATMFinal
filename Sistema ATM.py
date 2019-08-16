324a5f687dfd74fd7ad1f076aadefd018b4b0f10
niicoamorim <54079057+niicoamorim@users.noreply.github.com>
2019-08-16 15:51:38 -0300

    Add files via upload

--- Sistema ATM.py
+++ Sistema ATM.py
@@ -3,8 +3,9 @@ cpfcliente = []
 contacliente= []
 saldocliente = []
 inicio = 'S'
+tentativalogin = int(3)
 print('=' * 30)
-print('{:^30}'.format('NICOLAS BANK'))
+print('{:^30}'.format('BANCO DO RONEY BOY -  O MELHOR DE MACEIO'))
 print('=' * 30)
 
 while inicio == 'S':
@@ -27,7 +28,7 @@ while inicio == 'S':
 
 
     if verificacaocadastro == 'S':
-        print('Digite os dados abaixo para realizar login no sistema do banco:')
+        print(f'Digite os dados abaixo para realizar login no sistema do banco. Você ainda tem {tentativalogin} tentativas.')
         validacaocpf = str(input('Digite seu CPF:'))
         validacaoconta = str(input('Digite sua conta'))
 
@@ -35,15 +36,49 @@ while inicio == 'S':
             print('Bem vindo ao sistema do banco:')
             print('Escolha a operação que deseja realizar no sistema:')
             operacao = int(input(f'1 - Depósito. 2 - Saque. 3 - Saldo. 4 - Transferência. 5 - Sair '))
+
             if operacao == 1:
-              valordeposito = str(input('Digite o valor a ser depositado:'))
-              pos = validacaocpf.index(cpfcliente)
-              saldocliente[pos] += valordeposito
-              print(f'O saldo da conta {contacliente[pos]} agora é de R${saldocliente[pos]}.')
+                valordeposito = str(input('Digite o valor a ser depositado:'))
+                pos = validacaocpf.index(cpfcliente)
+                saldocliente[pos] += valordeposito
+                print(f'O saldo da conta {contacliente[pos]} agora é de R${saldocliente[pos]}.')
 
+            if operacao == 2:
+                valorsaque = int(input('Digite o valor a ser sacado:'))
+                pos = validacaoconta.index(contacliente)
+                if valorsaque > saldocliente[pos]:
+                    print('Saldo insuficente.')
+                else:
+                 saldocliente[pos] -= valorsaque
+                 print(f'O novo saldo da conta {contacliente[pos]} é {saldocliente[pos]}.')
 
+            if operacao == 3:
+                pos = validacaoconta.index(contacliente)
+                print(f'O saldo atual da conta {contacliente[pos]} é {saldocliente[pos]}')
+
+            if operacao == 4:
+                contatransferencia = str(input('Digite a conta a ser transferida:'))
+                valortransferencia = int(input('Digite o valor a ser transferido:'))
+                pos = validacaoconta.index(contacliente)
+                pos2 = contatransferencia.index(contacliente)
+                if valortransferencia > saldocliente[pos]:
+                    print('Vamor superior ao saldo da conta')
+                if valortransferencia <= saldocliente[pos]:
+                    saldocliente[pos] -= valortransferencia
+                    saldocliente[pos2] += valortransferencia
+                    print(f'Foi retirado R${valortransferencia} da conta {contacliente[pos]} e foi transferido para a conta {contacliente[pos2]}')
+
+            if operacao ==5:
+                print('Muito obrigado por acessa o sistema.')
+                break
 
         if validacaoconta not in contacliente:
             print('Conta não cadastrada.')
+            tentativalogin -= 1
         elif validacaocpf not in cpfcliente:
             print('CPF não cadastrado.')
+
+            tentativalogin -= 1
+        if tentativalogin < 0:
+            print('Você está bloqueado.\n Tente daqui há 30 minutos.')
+            break
